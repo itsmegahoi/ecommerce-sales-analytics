2d21ad25a019e664f5076fb533ec72e67906c273
Ashutosh Gahoi <gahoiashutosh99@gmail.com>
2025-09-24 02:16:39 +0530

    gold layers tables created for visualization

--- ecommerce_analytics/models/access/sales_summary.sql
+++ ecommerce_analytics/models/access/sales_summary.sql
@@ -6,13 +6,16 @@ with sales as (
         UnitPrice,
         discount,
         InvoiceDate,
+        transaction_type,
         (quantity * UnitPrice) - discount as net_sales
     from {{ ref('fact_sales') }}
 )
 
 select
     date_trunc('day', InvoiceDate) as order_date,
-    sum(net_sales) as total_sales,
+    sum(net_sales) as net_revenue,
+    SUM(CASE WHEN transaction_type = 'sale' THEN net_sales ELSE 0 END) AS total_sales,
+    SUM(CASE WHEN transaction_type = 'return' THEN net_sales ELSE 0 END) AS total_returns,
     count(distinct InvoiceNo) as total_orders,
     count(distinct CustomerID) as unique_customers,
     avg(net_sales) as avg_order_value
